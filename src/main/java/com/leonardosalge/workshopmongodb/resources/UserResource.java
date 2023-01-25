ee59255af7c3fffa6b157c1b5403bd0ed2fdddca
Leonardo Salge <lferreirasalge@gmail.com>
2023-01-25 13:58:39 -0300

    Obtendo usuario por ID, criando exceptions

--- src/main/java/com/leonardosalge/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/leonardosalge/workshopmongodb/resources/UserResource.java
@@ -6,6 +6,7 @@ import com.leonardosalge.workshopmongodb.dto.UserDTO;
 import com.leonardosalge.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RestController
@@ -27,5 +29,11 @@ public class UserResource {
         List<User> list = service.findAll();
         List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public ResponseEntity<UserDTO> findById(@PathVariable String id){
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(new UserDTO(obj));
     }
 }
--- src/main/java/com/leonardosalge/workshopmongodb/services/UserService.java
+++ src/main/java/com/leonardosalge/workshopmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.leonardosalge.workshopmongodb.services;
 
 import com.leonardosalge.workshopmongodb.domain.User;
 import com.leonardosalge.workshopmongodb.repository.UserRepository;
+import com.leonardosalge.workshopmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +19,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id){
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
     }
 }
