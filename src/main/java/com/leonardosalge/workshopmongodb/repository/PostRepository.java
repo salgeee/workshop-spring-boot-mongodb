f9efcd6fc1ccf7b30cc2ef974b63c8d4b16ad850
Leonardo Salge <lferreirasalge@gmail.com>
2023-01-26 10:00:03 -0300

    consulta com query methods

--- src/main/java/com/leonardosalge/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/leonardosalge/workshopmongodb/repository/PostRepository.java
@@ -1,10 +1,15 @@
 package com.leonardosalge.workshopmongodb.repository;
 
 import com.leonardosalge.workshopmongodb.domain.Post;
-import com.leonardosalge.workshopmongodb.domain.User;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+
+
+    List<Post> findByTitleContainingIgnoreCase(String text);
+
 }
--- src/main/java/com/leonardosalge/workshopmongodb/resources/PostResource.java
+++ src/main/java/com/leonardosalge/workshopmongodb/resources/PostResource.java
@@ -4,6 +4,7 @@ package com.leonardosalge.workshopmongodb.resources;
 import com.leonardosalge.workshopmongodb.domain.Post;
 import com.leonardosalge.workshopmongodb.domain.User;
 import com.leonardosalge.workshopmongodb.dto.UserDTO;
+import com.leonardosalge.workshopmongodb.resources.util.URL;
 import com.leonardosalge.workshopmongodb.services.PostService;
 import com.leonardosalge.workshopmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,13 @@ public class PostResource {
     public ResponseEntity<Post> findById(@PathVariable String id){
         Post obj = service.findById(id);
         return ResponseEntity.ok().body(obj);
+    }
+
+    @RequestMapping(value="/titlesearch", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findByTitle(@RequestParam(value="text", defaultValue="") String text) {
+        text = URL.decodeParam(text);
+        List<Post> list = service.findByTitle(text);
+        return ResponseEntity.ok().body(list);
     }
 
 }
