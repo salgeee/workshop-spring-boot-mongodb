98a289037b75ed84c7502459a331ee235e0f121d
Leonardo Salge <lferreirasalge@gmail.com>
2023-01-26 10:04:27 -0300

    Consulta simples com @Query

--- src/main/java/com/leonardosalge/workshopmongodb/repository/PostRepository.java
+++ src/main/java/com/leonardosalge/workshopmongodb/repository/PostRepository.java
@@ -2,6 +2,7 @@ package com.leonardosalge.workshopmongodb.repository;
 
 import com.leonardosalge.workshopmongodb.domain.Post;
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -11,5 +12,9 @@ public interface PostRepository extends MongoRepository<Post, String> {
 
 
     List<Post> findByTitleContainingIgnoreCase(String text);
+
+    @Query("{ 'title': { $regex: ?0, $options: 'i' } }")
+    List<Post> findByTitle(String text);
+
 
 }
--- src/main/java/com/leonardosalge/workshopmongodb/services/PostService.java
+++ src/main/java/com/leonardosalge/workshopmongodb/services/PostService.java
@@ -28,7 +28,7 @@ public class PostService {
     }
 
     public List<Post> findByTitle(String text) {
-        return repo.findByTitleContainingIgnoreCase(text);
+        return repo.findByTitle(text);
     }
 
 }
